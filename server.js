39f1d94a3ca1a979000204458f4af93174fe35e8
DavsLock <davidmusyoka499@gmail.com>
2026-06-26 18:21:34 +0300

    Fixed Render port configuration

--- server.js
+++ server.js
@@ -75,7 +75,8 @@ io.on("connection", (socket) => {
         console.log(`${socket.id} disconnected`);
     });
 });
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+server.listen(PORT, () => {
+    console.log("Server running on port ${PORT}");
 });
